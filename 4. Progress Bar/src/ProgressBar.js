152c83746209fc0eacff62ed64e7c9fc5f228aa1
Vyshnav <vyshnavk78@gmail.com>
2024-09-18 14:47:39 +0530

    useState, props, progressbar

--- 4. Progress Bar/src/ProgressBar.js
+++ 4. Progress Bar/src/ProgressBar.js
@@ -1,15 +1,14 @@
 import React from 'react'
 // width from the App.js. it should be the same variable name as App.js
-function ProgressBar({width}) {
-  
+function ProgressBar({ width }) {
+
   return (
     <>
-    <div className="container">
-    <div className="inner-container" style={{ width: `${width}%` }}>
-
-      {width}%
-    </div>
-    </div>
+      <div className="container">
+        <div className="inner-container" style={{ width: `${width}%` }}>
+          {width}%
+        </div>
+      </div>
     </>
   )
 }
