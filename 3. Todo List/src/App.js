7166abf4219f958da2e97dbd86c18cbd28f4454e
Vyshnav <vyshnavk78@gmail.com>
2024-09-18 14:48:24 +0530

    useState, map, filter, todo

--- 3. Todo List/src/App.js
+++ 3. Todo List/src/App.js
@@ -9,45 +9,45 @@ import "./styles.css";
 */
 
 export default function App() {
-// for taking input from the text input
+  // for taking input from the text input
   const [city, setCity] = useState("");
   // after the an input reaches the city variable, we need to add it to a list of cities
   const [cityList, setCityList] = useState([]);
 
   const addCities = () => {
     //adding city from input to the list
-    if(city==="") return 
+    if (city === "") return
     setCityList([...cityList, city])
     setCity("")  //clear the text field after clicking the button
   };
 
-  const removeCities = (id)=>{
-    setCityList(cityList.filter((_, i)=> i!== id))
+  const removeCities = (id) => {
+    setCityList(cityList.filter((_, i) => i !== id))
     // filter returns an array that matches the condition, i(index) is id
     // _ is each city and i is each index
   };
   console.log(cityList);
   return (
     <div className="App">
-      <input type="text" name="add-city" value={city} 
-      onChange={(e)=>{
-        setCity(e.target.value)
-      }}
-      placeholder="Add city here"/>
+      <input type="text" name="add-city" value={city}
+        onChange={(e) => {
+          setCity(e.target.value)
+        }}
+        placeholder="Add city here" />
       <span> </span>
       <button id="add-btn" onClick={addCities}>Add</button>
       <br />
       <div className="city-lists">
-      <ul>
-        {cityList.map((city, i)=>(
-          <li key={i}>{city}
-          <button id="del-btn" onClick={()=>removeCities(i)}>X</button>
-          </li>
-          //  if we called removeCitites directly did we did for addCitites then the
-          //  function will be executed when the component rendered,
-          //  resulting in the city being removed right after it was added.
-        ))}
-      </ul>
+        <ul>
+          {cityList.map((city, i) => (
+            <li key={i}>{city}
+              <button id="del-btn" onClick={() => removeCities(i)}>X</button>
+            </li>
+            //  if we called removeCitites directly did we did for addCitites then the
+            //  function will be executed when the component rendered,
+            //  resulting in the city being removed right after it was added.
+          ))}
+        </ul>
       </div>
     </div>
   );
